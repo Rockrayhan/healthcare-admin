8d66e339c53881b8e3f603cd145392a7f603c85c
rockRayhan <khayrulalamdict@gmail.com>
2024-01-04 15:27:48 +0600

    first commit

---
+++ resources/views/backend/category/create.blade.php
@@ -0,0 +1,11 @@
+@extends('backend.layouts.app')
+
+
+@section('content')
+<h1> All Category </h1>
+    <form action="">
+        Name: <input type="text">
+    </form>
+
+
+@endsection
---
+++ resources/views/backend/category/index.blade.php
@@ -0,0 +1,10 @@
+@extends('backend.layouts.app')
+
+
+@section('content')
+<h1> All Category </h1>
+ 
+
+
+
+@endsection
