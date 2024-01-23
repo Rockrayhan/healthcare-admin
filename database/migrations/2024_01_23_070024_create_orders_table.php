d97276eb015f8bc083a9507b9dd30ba758cb0710
rockRayhan <khayrulalamdict@gmail.com>
2024-01-23 13:12:11 +0600

    semifinal

---
+++ database/migrations/2024_01_23_070024_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('u_name' , 100);
+            $table->string('phone' , 50);
+            $table->string('p_name' , 50);
+            $table->string('quantity', 50);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
